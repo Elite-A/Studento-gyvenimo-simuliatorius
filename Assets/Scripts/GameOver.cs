1a89b9f2f010d88b882ec5b8a877844e14a22cd0
ChillWizardHat <201340731+ChillWizardHat@users.noreply.github.com>
2026-04-27 15:03:13 +0300

    Ui atnaujinimai

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -1,12 +1,12 @@
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
 using UnityEngine.SceneManagement;
 using System.ComponentModel.Design;
 using System.Xml.Linq;
 
 public class GameOver : MonoBehaviour
 {
-    public Text deathText;
+    public TMP_Text deathText;
     public void Restart()
     {
         SceneManager.LoadScene("Main");
