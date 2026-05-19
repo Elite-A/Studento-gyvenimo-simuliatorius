f925482378343b39d75f61bdccbbe9de59f0745d
ChillWizardHat <201340731+ChillWizardHat@users.noreply.github.com>
2026-05-19 20:20:00 +0300

    sutaisytas npc judejimas

--- Assets/Scripts/AiMovementScript.cs
+++ Assets/Scripts/AiMovementScript.cs
@@ -2,109 +2,82 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-
 public class AI_Movement : MonoBehaviour
 {
-
-   Animator animator;
-
-    //public so u can change it from inspector
+    Animator animator;
     public float moveSpeed = 0.2f;
-
-    //saves the stopped position 
     Vector3 stopPosition;
-
-    //randomly assigned time ai will walk
     float walkTime;
     public float walkCounter;
-    //randomly assigned time ai will wait
     float waitTime;
     public float waitCounter;
-
     int WalkDirection;
-
     public bool isWalking;
 
-    // Start is called before the first frame update
     void Start()
     {
-        //take previously created animator
         animator = GetComponent<Animator>();
-
-        //So that all the prefabs don't move/stop at the same time
-        walkTime = Random.Range(3,7);
-        waitTime = Random.Range(0,4);
-
-
+        walkTime = Random.Range(3, 7);
+        waitTime = Random.Range(0, 4);
         waitCounter = waitTime;
         walkCounter = walkTime;
-
         ChooseDirection();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (isWalking)
         {
-            //say its running
             animator.SetBool("isWalking", true);
-
             walkCounter -= Time.deltaTime;
 
             switch (WalkDirection)
-            //we check which direction we randomly chose
             {
                 case 0:
                     transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-                    transform.position += Vector3.back * moveSpeed * Time.deltaTime; 
+                    transform.position += Vector3.back * moveSpeed * Time.deltaTime;
                     break;
                 case 1:
                     transform.rotation = Quaternion.Euler(0f, 90f, 0f);
-                    transform.position += Vector3.left * moveSpeed * Time.deltaTime; 
+                    transform.position += Vector3.left * moveSpeed * Time.deltaTime;
                     break;
                 case 2:
                     transform.rotation = Quaternion.Euler(0f, -90f, 0f);
-                    transform.position += Vector3.right * moveSpeed * Time.deltaTime; 
+                    transform.position += Vector3.right * moveSpeed * Time.deltaTime;
                     break;
                 case 3:
                     transform.rotation = Quaternion.Euler(0f, 180f, 0f);
-                    transform.position += Vector3.forward * moveSpeed * Time.deltaTime; 
+                    transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
                     break;
             }
 
             if (walkCounter <= 0)
             {
-                stopPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+                stopPosition = transform.position;
                 isWalking = false;
-                //stop movement
                 transform.position = stopPosition;
-               animator.SetBool("isWalking", false);
-                //reset the waitCounter
+                animator.SetBool("isWalking", false);
                 waitCounter = waitTime;
             }
-
-
         }
         else
         {
-
             waitCounter -= Time.deltaTime;
-
             if (waitCounter <= 0)
-            {
                 ChooseDirection();
-            }
         }
     }
-
 
     public void ChooseDirection()
     {
         WalkDirection = Random.Range(0, 4);
-
         isWalking = true;
-        //we need to reset it every time we start walking
         walkCounter = walkTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Wall"))
+            ChooseDirection();
     }
 }
